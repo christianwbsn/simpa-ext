2efffe70dde1058c480805dc1ebddae867d5ef29
Christian Wibisono <christian.wibisono7@gmail.com>
2022-11-06 08:32:22 +0700

    init project

---
+++ src/routes/Home.tsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import logo from '../paper.png';
+import { ChromeMessage, Sender } from "../types";
+import { getCurrentTabUId, getCurrentTabUrl } from "../chrome/utils";
+
+export const Home = () => {
+    const [url, setUrl] = useState<string>('');
+    const [responseFromContent, setResponseFromContent] = useState<string>('');
+
+    let {push} = useHistory();
+
+    /**
+     * Get current URL
+     */
+    useEffect(() => {
+        getCurrentTabUrl((url) => {
+            setUrl(url || 'undefined');
+        })
+    }, []);
+
+    const sendTestMessage = () => {
+        const message: ChromeMessage = {
+            from: Sender.React,
+            message: "Hello from React",
+        }
+
+        getCurrentTabUId((id) => {
+            id && chrome.tabs.sendMessage(
+                id,
+                message,
+                (responseFromContentScript) => {
+                    setResponseFromContent(responseFromContentScript);
+                });
+        });
+    };
+
+    const sendRemoveMessage = () => {
+        const message: ChromeMessage = {
+            from: Sender.React,
+            message: "delete logo",
+        }
+
+        getCurrentTabUId((id) => {
+            id && chrome.tabs.sendMessage(
+                id,
+                message,
+                (response) => {
+                    setResponseFromContent(response);
+                });
+        });
+    };
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+                <p>Home</p>
+                <p>URL:</p>
+                <p>
+                    {url}
+                </p>
+                <button onClick={sendTestMessage}>SEND MESSAGE</button>
+                <button onClick={sendRemoveMessage}>Remove logo</button>
+                <p>Response from content:</p>
+                <p>
+                    {responseFromContent}
+                </p>
+                <button onClick={() => {
+                    push('/about')
+                }}>About page
+                </button>
+            </header>
+        </div>
+    )
+}

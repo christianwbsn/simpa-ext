b007fa2acdfea35f680bf6a82c92627b0ae966cd
Christian Wibisono <christian.wibisono7@gmail.com>
2022-11-06 09:36:26 +0700

    remove unused

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -16,17 +16,3 @@ chrome.runtime.onStartup.addListener(() => {
     console.log('[background.js] onStartup')
     alert('[background.js] onInstalled');
 });
-
-/**
- *  Sent to the event page just before it is unloaded.
- *  This gives the extension opportunity to do some clean up.
- *  Note that since the page is unloading,
- *  any asynchronous operations started while handling this event
- *  are not guaranteed to complete.
- *  If more activity for the event page occurs before it gets
- *  unloaded the onSuspendCanceled event will
- *  be sent and the page won't be unloaded. */
-chrome.runtime.onSuspend.addListener(() => {
-    console.log('[background.js] onSuspend')
-    alert('[background.js] onSuspend');
-});

8be6b280bdc3bcde505daa95cecb12de191bf18b
Christian Wibisono <christian.wibisono7@gmail.com>
2022-11-07 17:36:09 +0700

    parse paper id

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -9,6 +9,19 @@ const validateSender = (
     return sender.id === chrome.runtime.id && message.from === Sender.React;
 }
 
+const getPaperId =  (url: string) => {
+    const pdf_regexp = /arxiv.org\/[\S]*\/([^\/]*)$/;
+    url = url.replace(".pdf", "");
+    if (url.endsWith("/")) url = url.slice(0, -1);
+    var match;
+    match = url.match(pdf_regexp);
+    // The first match is the matched string, the second one is the captured group.
+    if (match === null || match.length !== 2) {
+      return null;
+    }
+    return match[1].slice(0, -2);
+}
+
 const messagesFromReactAppListener = (
     message: ChromeMessage,
     sender: chrome.runtime.MessageSender,
@@ -28,11 +41,12 @@ const messagesFromReactAppListener = (
 
     if (message.message == "get title") {
         const title = document.querySelector(".paper-title h1")?.innerHTML.trim();
-        const paperLink = document.querySelector(".paper-abstract a")?.getAttribute("href");
-        response([title, paperLink])
+        const paperLink = document.querySelector(".paper-abstract a")?.getAttribute("href")!;
+        response([title, getPaperId(paperLink)])
     }
 
 }
+
 
 
 const main = () => {
